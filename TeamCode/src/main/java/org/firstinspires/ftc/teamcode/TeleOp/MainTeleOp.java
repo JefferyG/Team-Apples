fff2b03d891c02d494dc70b0ef4dcd706d6146bc
sameekshaj19 <61807637+sameekshaj19@users.noreply.github.com>
2020-03-05 17:22:30 -0600

    New Turn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainTeleOp.java
@@ -47,16 +47,20 @@ public class MainTeleOp extends OpMode {
             rb.setPower(0);
             rf.setPower(0);
         }
-        if ((gamepad1.right_stick_x) >.1) {
-            lb.setPower(0.75);
-            lf.setPower(0.75);
+        if ((gamepad1.right_stick_x) < -.1) {
+            lb.setPower(1);
+            lf.setPower(1);
+            rb.setPower(-1);
+            rf.setPower(-1);
         } else {
             lb.setPower(0);
             lf.setPower(0);
         }
-       if ((gamepad1.right_stick_x)< -.1) {
-           rb.setPower(0.75);
-           rf.setPower(0.75);
+       if ((gamepad1.right_stick_x) > .1) {
+           rb.setPower(1);
+           rf.setPower(1);
+           lb.setPower(-1);
+           lf.setPower(-1);
        } else {
            rb.setPower(0);
            rf.setPower(0);
